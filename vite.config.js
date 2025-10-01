40e8074e4c3da8fafd72d8b4d7a0a1d9ee4c2bc1
Yongseok-2 <8465867@naver.com>
2025-10-01 18:46:45 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-    server: {
+  base: '/admin/', 
+  
+  server: {
     port: 7000,   // 원하는 포트 번호
     strictPort: true // 다른 프로세스가 사용 중이면 실패하고 종료
   },
